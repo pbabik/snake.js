2f625ce5cbcecfac2b40c74b0b434b145d51c8c9
Michal Mackiewicz <michalmacki@gmail.com>
2016-07-04 16:08:36 +0200

    Added <<in>> method for arrays and <<values>> for objects

--- snake.js
+++ snake.js
@@ -1,9 +1,9 @@
 /**
- * Snake.js v0.137
+ * Snake.js v0.138
  * Makes your JavaScript Parselmouth
  * @author Michał Mackiewicz <michalmacki@gmail.com>
  **/
-  
+
 var Snake = {
 
     /**
@@ -11,7 +11,7 @@ var Snake = {
      * @param {array} a_list - array to analyze
      * @returns {mixed} last - array element
      **/
-      
+
     last: function(a_list) {
         return a_list[a_list.length - 1];
     },
@@ -23,7 +23,7 @@ var Snake = {
      * @param {mixed} default_value - Value to return if specified key not found
      * @returns {mixed} - Value of specified object property, or given detault if not found
      **/
-      
+
     get: function(a_dict, a_key, default_value) {
         if(default_value === undefined) {
             default_value = null;
@@ -39,23 +39,23 @@ var Snake = {
      * Returns true if given string A ends with given string B, false otherwise
      * @param {string} - a_string String to test
      * @param {string} - a_beginning Beginning to test
-     * @returns {boolean} - Return boolean value 
+     * @returns {boolean} - Return boolean value
      **/
-      
+
     startswith: function(a_string, a_beginning) {
         if (a_string.indexOf(a_beginning) === 0) {
             return true;
         }
         return false;
     },
-    
+
     /**
      * Returns true if given string A ends with given string B, false otherwise
      * @param {string} a_string - String to test
      * @param {string} an_ending - Ending to test
-     * @returns {boolean} - Return boolean value 
+     * @returns {boolean} - Return boolean value
      **/
-     
+
     endswith: function(a_string, an_ending) {
         return a_string.substr(a_string.length - an_ending.length) == an_ending;
     },
@@ -65,8 +65,8 @@ var Snake = {
      * @param {array} keys - Array with keys
      * @param {array} values - Array with values
      * @returns {object} - Object with key-value pairs
-     **/ 
-    
+     **/
+
     zip: function(keys, values) {
         var result = {};
         for (var k = 0; k < keys.length; k++) {
@@ -83,8 +83,8 @@ var Snake = {
      * Converts any value to string if possible, or empty string if not
      * @param {mixed} - Input value
      * @returns {string} - Zero or more length string
-     **/ 
-    
+     **/
+
     str: function(mixed_input) {
         if (!mixed_input) {
             return '';
@@ -92,14 +92,14 @@ var Snake = {
             return mixed_input.toString();
         }
     },
-    
+
     /**
      * Converts any value to integer if possible, or given default value if not
      * @param {mixed} mixed_input - Input value
      * @param {integer} default_value - Default return value if input can't be converted to int, defaults to zero
      * @returns {integer} conv - Converted value
      **/
-      
+
     int: function(mixed_input,default_value) {
         var conv = parseInt(mixed_input);
         if(isNaN(conv)) {
@@ -107,13 +107,13 @@ var Snake = {
         }
         return conv;
     },
-    
+
     /**
      * Converts any value to float if possible, or given default value if not
      * @param {mixed} mixed_input - Input value
      * @param {integer} default_value - Default return value if input can't be converted to float, defaults to zero
      * @returns {float} - Converted value
-     **/    
+     **/
     _float: function(mixed_input,default_value) {
         var conv = parseFloat(this.str(mixed_input).replace(',', '.'));
             if(isNaN(conv)) {
@@ -121,25 +121,52 @@ var Snake = {
         }
         return conv;
     },
-    
+
     /**
      * Returns minimal value from array
      * @param {array} a_list - Array to analyze
      * @returns {float} - Minimal value
      */
-     
+
      min: function(a_list) {
          return Math.min.apply( Math, a_list );
      },
-     
+
     /**
      * Returns maximal value from array
      * @param {array} a_list - Array to analyze
      * @returns {float} - Maximal value
      */
-     
+
      max: function(a_list) {
           return Math.max.apply( Math, a_list );
+     },
+
+     /**
+     * Returns true if some item is present in array, false otherwise
+     * @param (array) a_list - Array to analyze
+     * @param {mixed} looked_value - Value to test
+     * @returns {boolean} true/false
+     **/
+
+     in: function(a_list,looked_value) {
+         return a_list.indexOf(looked_value) !== -1;
+     },
+
+     /**
+     * Returns all object own properties' values
+     * @param {object} Object to analyze
+     * @returns {array} Array of values
+     **/
+
+     values: function(a_dict) {
+         var return_list = [];
+         for(var key in a_dict) {
+             if(a_dict.hasOwnProperty(key)) {
+                 return_list.push(a_dict[key]);
+             }
+         }
+         return return_list;
      }
 }
 
--- test/spec/tests.js
+++ test/spec/tests.js
@@ -28,41 +28,53 @@ describe("Snake.js", function() {
             var compareObj = {'a':0.123,'b':4,'c':22,'d':177,'e':66.6,'f':-1,'g':-176,'h':-123.456};
             expect(myZip).toEqual(compareObj);
         });
+        it("should return true if element is present in array", function(){
+            var myBool = S.in(myArray,'h');
+            expect(myBool).toBe(true);
+        });
+        it("should return false if element is NOT present in array", function(){
+            var mySecondBool = S.in(myArray,'z');
+            expect(mySecondBool).toBe(false);
+        });
   });
-  
+
   describe("object methods", function() {
       var myObject;
       beforeEach(function() {
           myObject = {'a':1,'b':2,'c':3};
       });
-  
+
       it("should get defined property of an object",function() {
           var myGet = S.get(myObject,'b');
           expect(myGet).toEqual(2);
       });
-      
+
       it("should return null from get method, if property is not present and default_value not defined", function() {
           var myGet = S.get(myObject,'myProperty');
           expect(myGet).toBe(null);
       });
-      
+
       it("should return default_value from get method, if property is not present and default_value is defined", function() {
           var myGet = S.get(myObject,'myProperty',-1);
           expect(myGet).toEqual(-1);
       });
+      it("should return all object's values", function(){
+          var myValues = S.values(myObject);
+          expect(myValues).toEqual([1,2,3]);
+      })
 });
 
 describe("string methods", function() {
-   var myString; 
+   var myString;
   beforeEach(function() {
       myString = 'parselmouth';
   });
-    
+
   it("should return true from startswith method, if string starts with specified string", function() {
       var myStart = S.startswith(myString,'parsel');
       expect(myStart).toBe(true);
   });
-  
+
   it("should return false from startswith method, if string doesn't start with specified string", function() {
       var myStart = S.startswith(myString,'parsley');
       expect(myStart).toBe(false);
@@ -71,52 +83,52 @@ describe("string methods", function() {
       var myStart = S.endswith(myString,'mouth');
       expect(myStart).toBe(true);
   });
-  
+
   it("should return false from endswith method, if string doesn't end with specified string", function() {
       var myStart = S.endswith(myString,'moth');
       expect(myStart).toBe(false);
   });
-  
+
 });
 
 describe("conversion methods", function() {
-    
+
     it("should convert null to empty string", function() {
         expect(S.str(null)).toBe('');
     });
-    
+
     it("should convert undefined to empty string", function() {
         expect(S.str(undefined)).toBe('');
     });
-    
+
     it("should convert float to string", function() {
         expect(S.str(123.456)).toBe('123.456');
     });
-    
+
     it("should convert integer to string", function() {
         expect(S.str(123)).toBe('123');
     });
-    
+
     it("should convert non-parseInt value to zero by default", function() {
         expect(S.int(null)).toBe(0);
     });
-    
+
     it("should convert non-parseInt value to given integer value", function() {
         expect(S.int(null,-1)).toBe(-1);
     });
-    
+
     it("should convert comma-separated float string to real float",function() {
         expect(S._float('123,456')).toBe(123.456);
     });
-    
+
     it("should convert non-parseFloat value to zero by default", function() {
         expect(S._float(null)).toBe(0);
     });
-    
+
     it("should convert non-parseFloat value to given integer value", function() {
         expect(S._float(null,-1)).toBe(-1);
     });
 
 });
-  
+
 });
